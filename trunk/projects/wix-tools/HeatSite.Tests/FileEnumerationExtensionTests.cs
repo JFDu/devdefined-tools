2b9fe706d2ee8bcf11cc8abc1dd2d33355199ec3
bittercoder <bittercoder@d5b7db88-974e-0410-81c7-3958bac9dd38>
2008-06-29 11:26:13 +0000

    * Adding HeatSite - simple tool for extracting files from a website project into a wxs file.

---
+++ trunk/projects/wix-tools/HeatSite.Tests/FileEnumerationExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace HeatSite.Tests
+{
+    [TestFixture]
+    public class FileEnumerationExtensionTests
+    {
+        [Test]
+        public void ToRelative_WhenPathHasNoTrailingSpace_CorrectlySetsPath()
+        {
+            var files = new[] {"C:\\temp\\test.html"};
+            Assert.AreEqual("test.html", files.ToRelative("C:\\temp").First());
+        }
+
+        [Test]
+        public void ToRelative_WhenPathHasTrailingSpace_CorrectlySetsPath()
+        {
+            var files = new[] {"C:\\temp\\test.html"};
+            Assert.AreEqual("test.html", files.ToRelative("C:\\temp\\").First());
+        }
+    }
+}
---
+++ trunk/projects/wix-tools/HeatSite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommandLine;
+
+namespace HeatSite
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("HeatSite version: {0}", typeof (Program).Assembly.GetName().Version);
+
+            var distiller = new SiteDistiller();
+
+            if (Parser.ParseArgumentsWithUsage(args, distiller) == false)
+            {
+                Environment.Exit(2);
+            }
+
+            distiller.Execute();
+        }
+    }
+}
